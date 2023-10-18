0c4f567c97427a66ca15bc0d2eb22dd95211a754
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2023-10-18 09:41:56 -0300

    add config, train and __init__

---
+++ pyhardgen/config.py
@@ -0,0 +1,84 @@
+from dataclasses import dataclass
+from serde import serde
+from serde.yaml import from_yaml
+from schema import Schema, And, Optional
+from pathlib import Path
+import typing as t
+import yaml
+
+@serde
+@dataclass
+class model:
+    num_layers: int
+    features: list[int]
+    activation: str
+    loss: str
+
+@serde
+@dataclass
+class dataset:
+    filename: str
+    batch_size: int
+
+@serde
+@dataclass
+class optimizer:
+    name: str
+    lr: float
+    weight_decay: t.Optional[float] = None
+
+@serde
+@dataclass
+class Config:
+    name: str
+    ngpu: int
+    num_epochs: int
+    num_runs: int
+    num_workers: int
+    nn: model
+    dataset: dataset
+    optimizer: optimizer
+
+
+class nnSchema(Schema):
+
+    def validate(self, data, _is_event_schema=True):
+        data = super(nnSchema, self).validate(data, _is_event_schema=False)
+        if _is_event_schema and data['num_layers'] != len(data['features']):
+            raise ValueError('num_layers must be equal to the length of features')
+        return data
+
+
+SCHEMA = Schema({
+    'name': And(str, len),
+    'ngpu': And(int, lambda n: 0 <= n),
+    'num_epochs': And(int, lambda n: 0 < n),
+    'num_runs': And(int, lambda n: 0 < n),
+    'num_workers': And(int, lambda n: 0 <= n),
+    'nn': nnSchema({
+        'num_layers': And(int, lambda n: 0 < n),
+        'features': And([And(int, lambda n: 0 < n)], len),
+        'activation': And(str, len),
+        'loss': And(str, len),
+    }),
+    'dataset': {
+        'filename': And(str, len),
+        'batch_size': And(int, lambda n: 0 < n),
+    },
+    'optimizer': {
+        'name': And(str, len),
+        'lr': And(float, lambda n: 0 < n),
+        Optional('weight_decay'): And(float, lambda n: 0 <= n),
+    },
+})
+
+def load_config(path: Path) -> Config:
+    with open(path, 'r') as file:
+        config_raw_str = file.read()
+        config_raw = yaml.safe_load(config_raw_str)
+    
+    SCHEMA.validate(config_raw)
+
+    config = from_yaml(Config, config_raw_str)
+
+    return config
