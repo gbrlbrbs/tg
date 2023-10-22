418fd977aefee844ac606f0b2e487f022c923a69
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2023-10-22 15:34:14 -0300

    edit to return a dataframe

--- pyhardgen/datagen.py
+++ pyhardgen/datagen.py
@@ -1,22 +1,26 @@
 import torch
 from .nn.decoder import Decoder
 import numpy as np
+import pandas as pd
 
 def generate_data_from_z(
     decoder: Decoder,
     z_limits: np.ndarray,
+    columns: list[str],
     n_samples: int = 100,
-) -> np.ndarray:
+) -> pd.DataFrame:
     """Generate data from a decoder.
 
     Args:
         decoder (`Decoder`): Decoder.
         z_limits (`np.ndarray`): Limits for z. 2x2 array [z1, z2]
+        columns (`list[str]`): Columns for the dataframe.
         n_samples (int, optional): Number of samples. Defaults to 100.
     
     Returns:
-        np.ndarray: Generated data.
+        `pd.DataFrame`: Generated data.
     """
     points = np.random.uniform(z_limits[0], z_limits[1], size=(n_samples, 2))
     data: torch.Tensor = decoder(torch.from_numpy(points).float())
-    return data.detach().numpy()
+    df = pd.DataFrame(data.detach().numpy(), columns=columns)
+    return df
