25a60465f0060a50d3ff25611326622bdcd3e684
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2023-11-12 17:03:35 -0300

    exp1 + logs

--- experiments/exp1.py
+++ experiments/exp1.py
@@ -1,9 +1,10 @@
-from pyhardgen.train import train_encoder, train_decoder
+from pyhardgen.train import train_decoder
 from pyhardgen.config import load_config
 from pyhardgen.datagen import generate_data_from_z
-from pyhardgen.dataset import ProblemDataset, create_dataloader
-from pyhardgen.nn.encoder import Encoder
 from pyhardgen.nn.decoder import Decoder
+from pyhardgen.dataset import *
+from torch.utils.tensorboard.writer import SummaryWriter
+from datetime import datetime
 import torch
 import numpy as np
 import pandas as pd
@@ -14,25 +15,50 @@ def main():
     coordinates_path = data_path / 'coordinates.csv'
     dataset_path = data_path / 'sjc_internacao.csv'
     config_path = Path('../configs/exp1.yaml')
+    save_path = Path('./models/exp1')
+    save_path.mkdir(parents=True, exist_ok=True)
+    decoder_path = save_path / 'decoder.pt'
 
     config = load_config(config_path)
     device = torch.device('cuda' if torch.cuda.is_available() and (config.ngpu > 0) else 'cpu')
 
-    dataset = ProblemDataset(dataset_path, coordinates_path)
-    dataloader = create_dataloader(dataset, config.dataset.batch_size)
-    encoder = Encoder(dataset.n_features, 2, config.nn)
-    decoder = Decoder(2, dataset.n_features, config.nn)
+    data = pd.read_csv(dataset_path)
+    cats = data.drop(columns=["Age"]).columns.to_list()
+    conts = ["Age"]
+    dataset = ProblemDataset(dataset_path, coordinates_path, cats, conts)
+    dataloader = create_dataloader(dataset, config.dataset.batch_size, num_workers=config.workers)
+    cat_dict = {cat: len(data[cat].unique()) for cat in cats}
+    n_cat = sum(cat_dict.values())
+    n_cont = len(conts)
+    means = data[conts].mean().to_numpy()
+    stds = data[conts].std().to_numpy()
+    low = (data[conts].min().to_numpy() - means) / stds
+    high = (data[conts].max().to_numpy() - means) / stds
 
-    encoder_training = train_encoder(
-        dataloader,
-        encoder,
-        config,
-        device
-    )
+    decoder = Decoder(2, n_cont, n_cat, torch.tensor(low).to(device), torch.tensor(high).to(device), config.nn)
+    if decoder_path.exists():
+        decoder.load_state_dict(torch.load(decoder_path))
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    writer = SummaryWriter("./logs/exp1/" + timestamp)
+    
+    if not decoder_path.exists():
+        decoder_training = train_decoder(
+            dataloader,
+            decoder,
+            config,
+            device,
+            writer,
+            cat_dict
+        )
+        torch.save(decoder_training.model.state_dict(), save_path / 'decoder.pt')
 
-    decoder_training = train_decoder(
-        dataloader,
-        decoder,
-        config,
-        device
-    )
+    z1_limits = np.array([-2.5, -1.5]).reshape((2, 1))
+    z2_limits = np.array([-1.0, 0.0]).reshape((2, 1))
+    z_limits = np.hstack((z1_limits, z2_limits))
+    columns = data.columns.to_list()
+    # df = generate_data_from_z(decoder, z_limits, columns, device=device, n_samples=1000)
+    # df.to_csv(data_path / 'generated_data.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
